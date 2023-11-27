c355e8d9d9e4383819461a445c211ed4c432264b
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-27 08:36:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@ rm -rf feeds/packages/net/v2ray-core
 rm -rf feeds/packages/net/v2ray-plugin
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/xray-plugin
-rm -rf feeds/kiddin9/shortcut-fe
+
 #sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
